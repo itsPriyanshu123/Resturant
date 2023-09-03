40685bf9a9105fa9d37846b785828f87d4f1b33d
itsPriyanshu123 <priyanshujee2017@gmail.com>
2023-09-03 10:52:07 -0700

    ep5

---
+++ Components/Header.js
@@ -0,0 +1,28 @@
+import logoImage from "/common/logo.jpg";
+// Title component for display logo
+const Title = () => (
+  <a href="/">
+    <img className="logo" src={logoImage} alt="Food Fire Logo" />
+  </a>
+);
+
+// Header component for header section: Logo, Nav Items
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Components/RestaurantCard.js
@@ -0,0 +1,34 @@
+
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  cuisines,
+  area,
+  lastMileTravelString,
+  costForTwoString,
+  avgRating,
+}) => {
+  return (
+    <div className="card">
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" +
+             cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h5>{cuisines.join(", ")}</h5>
+      <h6>{area}</h6>
+      <span>
+        <h4
+          style={
+            avgRating < 4 ? { backgroundColor: "red" } : { color: "white" }
+          }
+        >
+          <i className="fa-solid fa-star"></i>
+          {avgRating}
+        </h4>
+        <h4>{lastMileTravelString}</h4>
+        <h4>{costForTwoString}</h4>
+      </span>
+    </div>
+  );
+};
+
+export default RestaurantCard;
